be19052e58c962fe36c7e0fd26c49ec3640b908e
Fitalrk800 <fitalgames@gmail.com>
2021-06-04 19:49:06 +0500

    add menu

---
+++ 3D_Rythm_Game/Assets/_GameFiles/_Scripts/MainMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame() 
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
